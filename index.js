e6665c7a718eb255e0e1699f18ce7ca78ffb27e4
Sunibha Maskey <sunibhamaskey1@gmail.com>
2026-06-30 20:20:27 +0545

    All necessary changes made.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+const clockFace = document.getElementById("clock-face");
+
+
+for (let i = 1; i <= 12; i++) {
+    const number = document.createElement("div");
+    number.className = `number number${i}`;
+    number.textContent = i;
+    clockFace.appendChild(number);
+}
+
+
+const hourHand = document.getElementById("hour-hand");
+const minuteHand = document.getElementById("minute-hand");
+const secondHand = document.getElementById("second-hand");
+
+
 function setClock() {
+
     const now = new Date();
+
     const second = now.getSeconds();
     const minute = now.getMinutes();
     const hour = now.getHours();
 
-    const secondDeg = ((second / 60) * 360) + 90;
-    const minuteDeg = ((minute + second / 60) / 60) * 360 + 90;
-    const hourDeg = ((hour % 12 + minute / 60) / 12) * 360 + 90;
 
-    document.getElementById("second-hand").style.transform = `rotate(${secondDeg}deg)`;
-    document.getElementById("minute-hand").style.transform = `rotate(${minuteDeg}deg)`;
-    document.getElementById("hour-hand").style.transform = `rotate(${hourDeg}deg)`;
+    const secondDeg = second * 6 + 90;
+    const minuteDeg = (minute + second / 60) * 6 + 90;
+    const hourDeg = (hour % 12 + minute / 60) * 30 + 90;
+
+
+    secondHand.style.transform = `rotate(${secondDeg}deg)`;
+    minuteHand.style.transform = `rotate(${minuteDeg}deg)`;
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
 }
+
 
 setClock();
 setInterval(setClock, 1000);
